ad02e1a5b484cc90149134de47cbdc416318226b
ezzygarmyz <dragkhai@gmail.com>
2025-03-03 04:03:18 +0100

    wallet interface

---
+++ btczwallet/resources/menu.py
@@ -0,0 +1,263 @@
+
+import asyncio
+
+from toga import (
+    Window, Box, Button
+)
+from toga.style.pack import Pack
+from toga.colors import WHITE, YELLOW, BLACK, GRAY
+from toga.constants import (
+    COLUMN, ROW, TOP, CENTER, BOLD
+)
+
+from .client import Client
+from .utils import Utils
+from .wallet import Wallet
+from .home import Home
+from .recieve import Recieve
+from .send import Send
+from .status import AppStatusBar
+
+class Menu(Window):
+    def __init__(self):
+        super().__init__()
+
+        self.commands = Client(self.app)
+        self.utils = Utils(self.app)
+        self.wallet = Wallet(self.app)
+        self.statusbar = AppStatusBar(self.app)
+
+        self.title = "BitcoinZ Wallet"
+        self.size = (900,600)
+        
+        self._is_minimized = None
+        
+        position_center = self.utils.windows_screen_center(self.size)
+        self.position = position_center
+        self.on_close = self.on_close_menu
+
+        self.main_box = Box(
+            style=Pack(
+                direction = COLUMN,
+                flex = 1,
+                alignment = CENTER
+            )
+        )
+        self.menu_bar = Box(
+            style=Pack(
+                direction = ROW,
+                alignment = TOP,
+                height = 45,
+                flex = 1,
+                padding = (0,4,5,4)
+            )
+        )
+        self.pages = Box(
+            style=Pack(
+                direction = COLUMN,
+                flex = 1
+            )
+        )
+
+        self.home_page = Home(self.app)
+        self.recieve_page = Recieve(self.app, self)
+        self.send_page = Send(self.app, self)
+
+        self.main_box.add(
+            self.wallet,
+            self.menu_bar,
+            self.pages,
+            self.statusbar
+        )
+
+        self.content = self.main_box
+
+        self.insert_menu_buttons()
+
+    def insert_menu_buttons(self):
+        self.home_button = Button(
+            text="Home",
+            style=Pack(
+                font_weight = BOLD,
+                color = GRAY,
+                flex = 1,
+                padding = (5,0,2,5)
+            ),
+            on_press=self.home_button_click
+        )
+
+        self.transactions_button = Button(
+            text="Transactions",
+            style=Pack(
+                font_weight = BOLD,
+                color = GRAY,
+                flex = 1,
+                padding = (5,0,2,0)
+            ),
+            on_press=self.transactions_button_click
+        )
+
+        self.recieve_button = Button(
+            text="Recieve",
+            style=Pack(
+                font_weight = BOLD,
+                color = GRAY,
+                flex = 1,
+                padding = (5,0,2,0)
+            ),
+            on_press=self.recieve_button_click
+        )
+
+        self.send_button = Button(
+            text="Send",
+            style=Pack(
+                font_weight = BOLD,
+                color = GRAY,
+                flex = 1,
+                padding = (5,0,2,0)
+            ),
+            on_press=self.send_button_click
+        )
+
+        self.message_button = Button(
+            text="Messages",
+            style=Pack(
+                font_weight = BOLD,
+                color = GRAY,
+                flex = 1,
+                padding = (5,0,2,0)
+            ),
+            on_press=self.message_button_click
+        )
+        
+        self.mining_button = Button(
+            text="Mining",
+            style=Pack(
+                font_weight = BOLD,
+                color = GRAY,
+                flex = 1,
+                padding = (5,5,2,0)
+            ),
+            on_press=self.mining_button_click
+        )
+
+        self.menu_bar.add(
+            self.home_button,
+            self.transactions_button,
+            self.recieve_button,
+            self.send_button,
+            self.message_button,
+            self.mining_button
+        )
+
+        self.home_button_toggle = None
+        self.transactions_button_toggle = None
+        self.recieve_button_toggle = None
+        self.send_button_toggle = None
+        self.message_button_toggle = None
+        self.mining_button_toggle = None
+        self.app.add_background_task(self.set_default_page)
+
+
+    async def set_default_page(self, widget):
+        await asyncio.sleep(0.5)
+        self.home_button_click(None)
+
+
+    def home_button_click(self, button):
+        self.clear_buttons()
+        self.home_button_toggle = True
+        self.home_button.style.color = BLACK
+        self.home_button.style.background_color = YELLOW
+        self.home_button.on_press = None
+        self.pages.add(self.home_page)
+        self.app.add_background_task(self.home_page.insert_widgets)
+
+
+    def transactions_button_click(self, button):
+        self.clear_buttons()
+        self.transactions_button_toggle = True
+        self.transactions_button.style.color = BLACK
+        self.transactions_button.style.background_color = YELLOW
+        self.transactions_button.on_press = None
+
+
+    def recieve_button_click(self, button):
+        self.clear_buttons()
+        self.recieve_button_toggle = True
+        self.recieve_button.style.color = BLACK
+        self.recieve_button.style.background_color = YELLOW
+        self.recieve_button.on_press = None
+        self.pages.add(self.recieve_page)
+        self.app.add_background_task(self.recieve_page.insert_widgets)
+
+
+    def send_button_click(self, button):
+        self.clear_buttons()
+        self.send_button_toggle = True
+        self.send_button.style.color = BLACK
+        self.send_button.style.background_color = YELLOW
+        self.send_button.on_press = None
+        self.pages.add(self.send_page)
+        self.app.add_background_task(self.send_page.insert_widgets)
+
+    
+    def message_button_click(self, button):
+        self.clear_buttons()
+        self.message_button_toggle = True
+        self.message_button.style.color = BLACK
+        self.message_button.style.background_color = YELLOW
+        self.message_button.on_press = None
+
+
+    def mining_button_click(self, button):
+        self.clear_buttons()
+        self.mining_button_toggle = True
+        self.mining_button.style.color = BLACK
+        self.mining_button.style.background_color = YELLOW
+        self.mining_button.on_press = None
+
+
+    def clear_buttons(self):
+        if self.home_button_toggle:
+            self.home_button_toggle = None
+            self.pages.remove(self.home_page)
+            self.home_button.style.color = GRAY
+            self.home_button.style.background_color = WHITE
+            self.home_button.on_press = self.home_button_click
+
+        elif self.transactions_button_toggle:
+            self.transactions_button_toggle = None
+            self.transactions_button.style.color = GRAY
+            self.transactions_button.style.background_color = WHITE
+            self.transactions_button.on_press = self.transactions_button_click
+
+        elif self.recieve_button_toggle:
+            self.recieve_button_toggle = None
+            self.pages.remove(self.recieve_page)
+            self.recieve_button.style.color = GRAY
+            self.recieve_button.style.background_color = WHITE
+            self.recieve_button.on_press = self.recieve_button_click
+        
+        elif self.send_button_toggle:
+            self.send_button_toggle = None
+            self.pages.remove(self.send_page)
+            self.send_button.style.color = GRAY
+            self.send_button.style.background_color = WHITE
+            self.send_button.on_press = self.send_button_click
+
+        elif self.message_button_toggle:
+            self.message_button_toggle = None
+            self.message_button.style.color = GRAY
+            self.message_button.style.background_color = WHITE
+            self.message_button.on_press = self.message_button_click
+
+        elif self.mining_button_toggle:
+            self.mining_button_toggle = None
+            self.mining_button.style.color = GRAY
+            self.mining_button.style.background_color = WHITE
+            self.mining_button.on_press = self.message_button_click
+            
+
+    def on_close_menu(self, widget):
+        self.app.exit()
---
+++ btczwallet/resources/wallet.py
@@ -0,0 +1,227 @@
+
+import asyncio
+import json
+
+from toga import App, Box, Label, ImageView
+from toga.style.pack import Pack
+from toga.colors import rgb, GRAY, RED, BLACK
+from toga.constants import (
+    TOP, ROW, LEFT, BOLD, COLUMN,
+    RIGHT, CENTER, BOTTOM
+)
+
+from .client import Client
+from .utils import Utils
+
+class Wallet(Box):
+    def __init__(self, app:App):
+        super().__init__(
+            style=Pack(
+                direction = ROW,
+                height = 120,
+                alignment = TOP,
+                padding =5
+            )
+        )
+
+        self.app = app
+        self.commands = Client(self.app)
+        self.utils = Utils(self.app)
+
+        self.unconfirmed_balance_toggle = None
+
+        self.bitcoinz_logo = ImageView(
+            image="images/BTCZ.png",
+            style=Pack(
+                width=100,
+                height=100,
+                padding_top = 10,
+                padding_left = 10
+            )
+        )
+        self.bitcoinz_title = Label(
+            text="Full Node Wallet",
+            style=Pack(
+                color = GRAY,
+                font_size = 20,
+                font_weight = BOLD,
+                text_align = LEFT,
+                flex = 1,
+                padding_top = 40
+            )
+        )
+        self.balances_box = Box(
+            style=Pack(
+                direction = COLUMN,
+                background_color = rgb(230,230,230),
+                padding = 10,
+                alignment = RIGHT,
+                width = 350
+            )
+        )
+        self.total_balances_label = Label(
+            text="Total Balances",
+            style=Pack(
+                font_size = 13,
+                font_weight = BOLD,
+                text_align = CENTER,
+                color = GRAY,
+                padding_top = 5,
+                flex =1
+            )
+        )
+        self.total_value = Label(
+            text="",
+            style=Pack(
+                font_size = 13,
+                font_weight = BOLD,
+                text_align = CENTER,
+                color = BLACK,
+                padding_top = 5
+            )
+        )
+        self.balances_type_box = Box(
+            style=Pack(
+                direction = ROW,
+                alignment = BOTTOM,
+                flex = 1
+            )
+        )
+
+        self.transparent_balance_box = Box(
+            style=Pack(
+                direction = COLUMN,
+                padding = 5,
+                flex = 1
+            )
+        )
+        self.private_balance_box = Box(
+            style=Pack(
+                direction = COLUMN,
+                padding = 5,
+                flex = 1
+            )
+        )
+
+        self.transparent_label = Label(
+            text="Transparent",
+            style=Pack(
+                text_align = CENTER,
+                color = GRAY,
+                font_weight = BOLD
+            )
+        )
+
+        self.transparent_value = Label(
+            text="",
+            style=Pack(
+                text_align = CENTER,
+                color = BLACK,
+                font_weight = BOLD
+            )
+        )
+
+        self.private_label = Label(
+            text="Private",
+            style=Pack(
+                text_align = CENTER,
+                color = GRAY,
+                font_weight = BOLD
+            )
+        )
+
+        self.private_value = Label(
+            text="",
+            style=Pack(
+                text_align = CENTER,
+                color = rgb(114,137,218),
+                font_weight = BOLD
+            )
+        )
+        self.unconfirmed_label = Label(
+            text="Unconfirmed Balance",
+            style=Pack(
+                text_align = CENTER,
+                color = GRAY,
+                font_weight = BOLD,
+                padding_top = 5
+            )
+        )
+        self.unconfirmed_value = Label(
+            text="",
+            style=Pack(
+                text_align = CENTER,
+                color = RED,
+                font_weight = BOLD,
+                padding_bottom = 5
+            )
+        )
+        self.unconfirmed_box = Box(
+            style=Pack(
+                direction = COLUMN,
+                alignment = CENTER,
+                padding_top = 70
+            )
+        )
+
+        self.add(
+            self.bitcoinz_logo,
+            self.bitcoinz_title,
+            self.balances_box
+        )
+
+        self.balances_box.add(
+            self.total_balances_label,
+            self.total_value,
+            self.balances_type_box
+        )
+
+        self.balances_type_box.add(
+            self.transparent_balance_box,
+            self.private_balance_box
+        )
+
+        self.transparent_balance_box.add(
+            self.transparent_label,
+            self.transparent_value
+        )
+        self.private_balance_box.add(
+            self.private_label,
+            self.private_value
+        )
+        self.app.add_background_task(self.update_balances)
+
+
+    async def update_balances(self, widget):
+        while True:
+            totalbalances,_ = await self.commands.z_getTotalBalance()
+            if totalbalances is not None:
+                balances = json.loads(totalbalances)
+                totalbalance = self.utils.format_balance(float(balances.get('total')))
+                transparentbalance = self.utils.format_balance(float(balances.get('transparent')))
+                privatebalance = self.utils.format_balance(float(balances.get('private')))
+                self.total_value.text = totalbalance
+                self.transparent_value.text = transparentbalance
+                self.private_value.text = privatebalance
+            unconfirmed_balance,_ = await self.commands.getUnconfirmedBalance()
+            if unconfirmed_balance is not None:
+                unconfirmed = self.utils.format_balance(float(unconfirmed_balance))
+                if float(unconfirmed) > 0:
+                    if not self.unconfirmed_balance_toggle:
+                        self.insert(2, self.unconfirmed_box)
+                        self.unconfirmed_box.add(
+                            self.unconfirmed_label,
+                            self.unconfirmed_value
+                        )
+                        self.unconfirmed_balance_toggle = True
+                    self.unconfirmed_value.text = unconfirmed
+                else:
+                    if self.unconfirmed_balance_toggle:
+                        self.unconfirmed_box.remove(
+                            self.unconfirmed_label,
+                            self.unconfirmed_value
+                        )
+                        self.remove(self.unconfirmed_box)
+                        self.unconfirmed_balance_toggle = False
+                    
+            await asyncio.sleep(5)
